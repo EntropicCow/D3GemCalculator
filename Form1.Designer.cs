ee89b868083596681c763a69eb7425f7c483ea15
Jose A. Araujo <entropiccow@gmail.com>
2015-04-08 12:23:41 -0500

    Initial Comment
    Initial Commit of this project.

---
+++ Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace D3GemCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Aboutbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.comboBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboBox1.DropDownWidth = 107;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Chipped",
+            "Flawed",
+            "Normal",
+            "Flawless",
+            "Perfect",
+            "Radiant",
+            "Square",
+            "Flawless Square",
+            "Perfect Square",
+            "Radiant Square",
+            "Star",
+            "Flawless Star",
+            "Perfect Star",
+            "Radiant Star",
+            "Marquise",
+            "Imperial",
+            "Flawless Imperial",
+            "Royal",
+            "Flawless Royal"});
+            this.comboBox1.Location = new System.Drawing.Point(1, 25);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(113, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.comboBox2.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Chipped",
+            "Flawed",
+            "Normal",
+            "Flawless",
+            "Perfect",
+            "Radiant",
+            "Square",
+            "Flawless Square",
+            "Perfect Square",
+            "Radiant Square",
+            "Star",
+            "Flawless Star",
+            "Perfect Star",
+            "Radiant Star",
+            "Marquise",
+            "Imperial",
+            "Flawless Imperial",
+            "Royal",
+            "Flawless Royal"});
+            this.comboBox2.Location = new System.Drawing.Point(120, 25);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(107, 21);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(120, 52);
+            this.textBox1.MaxLength = 6;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(45, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Amount";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Calculate!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(-2, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Gem Used";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(117, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Desired Gem";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.Location = new System.Drawing.Point(1, 107);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.listBox1.Size = new System.Drawing.Size(226, 95);
+            this.listBox1.TabIndex = 8;
+            // 
+            // Aboutbutton
+            // 
+            this.Aboutbutton.Location = new System.Drawing.Point(152, 78);
+            this.Aboutbutton.Name = "Aboutbutton";
+            this.Aboutbutton.Size = new System.Drawing.Size(75, 23);
+            this.Aboutbutton.TabIndex = 9;
+            this.Aboutbutton.Text = "About";
+            this.Aboutbutton.UseVisualStyleBackColor = true;
+            this.Aboutbutton.Click += new System.EventHandler(this.Aboutbutton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(228, 203);
+            this.Controls.Add(this.Aboutbutton);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "D3 Gem Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button Aboutbutton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+
+namespace D3GemCalculator
+{
+    public partial class Form1 : Form
+    {
+        //instantiate the gem class as "GemCalculator" using "new" to invoke constructor and default values
+        Gem GemCalculator = new Gem();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        { 
+            listBox1.Items.Clear(); // clear the listbox everytime the calculate button is pressed
+
+            string[] output;
+            output = GemCalculator.SanityCheck(comboBox1.SelectedIndex, comboBox2.SelectedIndex, textBox1.Text, comboBox1.SelectedItem, comboBox2.SelectedItem);
+
+            foreach (string message in output)
+            {
+                if(message == "")
+                {
+                    ; //no deathsbreath text, so do nothing.
+                }
+                else
+                listBox1.Items.Add(message);
+            }
+        }
+
+        private void Aboutbutton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Programmed by Jose A. Araujo 2014 \nThanks to Dale and William for inspiration.", "Diablo 3 Gem Calculator");
+        }
+    }
+}
